ff7414b1aeb3d65cc664f943f732b7f514b319a7
Tiago Cunha <tcunha_lp@hotmail.com>
2019-04-21 23:40:30 +0100

    config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,14 @@ class UsersController < ApiController
 
 	def profile
 		user = User.find_by_auth_token!(request.headers[ :token ])
-		render json: { user: { username: user.username, email: user.email, first_name: user.first_name, last_name: user.last_name }}
+		render json: { user: { id: user.id, username: user.username, email: user.email, first_name: user.first_name, last_name: user.last_name }}
 	end
+
+	def destroy
+		user = User.find_by_auth_token!(request.headers[ :token ])
+    	user.destroy
+    	render json: { status: :ok }
+  	end
 
 	private
 
