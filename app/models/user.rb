960b39f3e70fefbad0723e9edcd3fe6649159b30
Tiago Cunha <tcunha_lp@hotmail.com>
2019-05-08 12:39:15 +0100

    file validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
 	validates_uniqueness_of :email
 	validates_presence_of :first_name, :last_name
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "has invalid format!" }
-	
+	validate :validate_file
+
 	has_secure_password
 	has_secure_token :auth_token
 
@@ -19,5 +20,15 @@ class User < ApplicationRecord
 		if user && user.authenticate(password)
 			user
 		end
+	end
+
+	def validate_file
+		if govid.attached? && !govid.content_type.in?(%w(image/jpeg image/png application/pdf))
+			govid.purge
+			errors.add(:govid, "Invalid file format")
+		elsif govid.attached? === false
+			errors.add(:govid, "File must be attached")
+		end
+
 	end
 end
