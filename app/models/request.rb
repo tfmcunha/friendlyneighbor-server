0bf428b20fa2a5aef647394e37d0ffab4cf5a90d
Tiago Cunha <tcunha_lp@hotmail.com>
2019-05-08 12:38:54 +0100

    request validation added

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
   belongs_to :user
   has_many :volunteers, :dependent => :delete_all  #falta migrar para db
+
+  validates_presence_of :title, :body, :req_type
 end
