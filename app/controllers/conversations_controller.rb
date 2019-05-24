3077d34d9c3c461b687b7e5975171292946a0896
Tiago Cunha <tcunha_lp@hotmail.com>
2019-05-24 22:21:34 +0100

    visibility manager

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,15 +1,20 @@
 class ConversationsController < ApiController
 
 	def create
+
         users = [conversation_params[:recipient_id], conversation_params[:sender_id]]
         conversation = Conversation.where({request_id: conversation_params[:request_id]}).where({sender_id: [users]}).where({recipient_id: [users]}).first       
+        request = Request.find_by_id(conversation_params[:request_id])
 
-        if conversation != nil             
-            render json: {conversation_id: conversation.id}
-        else
-            conversation = Conversation.create!(conversation_params)   
-            Volunteer.create(user_id: conversation_params[:sender_id], request_id: conversation_params[:request_id])
-            render json: {conversation_id: conversation.id}
+        if conversation_params[:recipient_id] != conversation_params[:sender_id]  
+            if conversation != nil             
+                render json: {conversation_id: conversation.id}
+            else
+                conversation = Conversation.create!(conversation_params)   
+                Volunteer.create(user_id: conversation_params[:sender_id], request_id: conversation_params[:request_id])
+                request.visibility
+                render json: {conversation_id: conversation.id}
+            end
         end
     end
 
