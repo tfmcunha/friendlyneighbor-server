f4ca9114929843001db9208f583edf03a454274a
Tiago Cunha <tcunha_lp@hotmail.com>
2019-05-31 12:22:55 +0100

    request visibility management

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,12 @@
 class Volunteer < ApplicationRecord
   belongs_to :user
-  has_many :requests
+  belongs_to :request
 
+  after_create :visibility
+
+  def visibility
+    if self.request.volunteers.count >= 5
+    	self.request.update_attributes(:visible => false)
+  	end   
+  end
 end
